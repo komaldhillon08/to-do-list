07550fa8209e9e4110a73ae1f1f7797c23b95d77
Sandeep <your-email@example.com>
2025-03-31 12:47:50 +0530

    add new code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
 console.log("komal");
+const inputBox = document.getElementById("input-box") ;
+const listContainer = document.getElementById("list-container") ;
+let btn = document.getElementById("add-btn");
+
+btn.addEventListener('click' , () =>{
+
+    if (inputBox.value === '' ) {
+        alert("You must write something !");
+    }
+    else{
+        let li = document.createElement('li') ; 
+        li.innerHTML = inputBox.value ; 
+        listContainer.appendChild(li);
+        // create the span 
+        let span = document.createElement('span') ; 
+        span.innerHTML = "\u00d7" ; 
+        li.appendChild(span) ; 
+     
+        inputBox.value = '';
+    }
+   
+}) ; 
+
+listContainer.addEventListener('click' , function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked"); 
+    }
+    else if(e.target.tagName === "SPAN")
+        e.target.parentElement.remove(); 
+    } , false )
+
+ 
